c7cb771d28ed66a5ac24e26b1b092b331a07be4a
OCPTECH S.A. <wxcc0000000@gmail.com>
2026-07-31 12:09:11 -0300

    Update app.js

--- gold/js/app.js
+++ gold/js/app.js
@@ -11,13 +11,19 @@
 
   Para producción, no expongas el token de la Service App en el navegador.
   La generación de guest token y call token debería hacerse desde un backend.
-*/
+//TOK JCA
 let service_app_token ='NjUzNWZhMzAtMTdmMC00NmI1LWIzMWItYjk4Y2MyYzg3ZWZkM2Q0MzI1M2MtMzli_PF84_8c3c67f5-9dba-4be8-b058-76d7afe45521';
 let service_app_refresh_token = 'RjlkNzgwZmYtY2I4NC00ZjJjLWIwZDMtODg0N2M2MzczOTVjODJkMmVjMTktNDI2_PF84_8c3c67f5-9dba-4be8-b058-76d7afe45521';
 const service_app_client_id = 'Ca24a93779a32b746d84f9b5e3f6638c964b1cd39ad1016d2f9cdf1f469edf4f8';
 const service_app_client_secret ='1196b6748e68234410bd217add920c1d8afe295d5cbf05b3143855a9240bdb67';
+*/
+//TOKE CARL
+let service_app_token = 'NTRjNTBjZGEtOGM5ZC00NjVjLWIzNDctOTZmZWIzOGZkYzA5ZjFjN2U5MzAtMDRk_P0A1_13ab0633-3ac9-4201-86fb-b00be6f71b9c';
+let service_app_refresh_token = 'RDZkM2U0YmMtMzgxOS00YmUwLTk4MTYtNWY1NzAzMzc5MWUzOTg4ZWY3NjgtMjM2_P0A1_13ab0633-3ac9-4201-86fb-b00be6f71b9c';
+const service_app_client_id = 'Ca54f9127b200556fec167b0fd96db5cad625d20ba3de510d40ad6718af6699c8';
+const service_app_client_secret = 'c6af6c268a29b72348c18361f205e22086406cd4a97de8e45c0c9d3969fdd671';
 
-const CLICK_TO_CALL_CALLED_NUMBER = '0676';
+const CLICK_TO_CALL_CALLED_NUMBER = '9699';
 const CLICK_TO_CALL_GUEST_NAME = 'soporte web';
 const WEBEX_DISCOVERY_REGION = 'US-EAST';
 const WEBEX_DISCOVERY_COUNTRY = 'US';

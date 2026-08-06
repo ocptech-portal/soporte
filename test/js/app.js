0e1ecf308c6b047ac435879794d67db175f48b7b
OCPTECH S.A. <wxcc0000000@gmail.com>
2026-08-06 15:54:55 -0300

    Update app.js

--- test/js/app.js
+++ test/js/app.js
@@ -15,11 +15,11 @@
   tok carl
 */
 
-let service_app_token = 'NTRjNTBjZGEtOGM5ZC00NjVjLWIzNDctOTZmZWIzOGZkYzA5ZjFjN2U5MzAtMDRk_P0A1_13ab0633-3ac9-4201-86fb-b00be6f71b9c';
-let service_app_refresh_token = 'RDZkM2U0YmMtMzgxOS00YmUwLTk4MTYtNWY1NzAzMzc5MWUzOTg4ZWY3NjgtMjM2_P0A1_13ab0633-3ac9-4201-86fb-b00be6f71b9c';
-const service_app_client_id = 'Ca54f9127b200556fec167b0fd96db5cad625d20ba3de510d40ad6718af6699c8';
-const service_app_client_secret = 'c6af6c268a29b72348c18361f205e22086406cd4a97de8e45c0c9d3969fdd671';
-const CLICK_TO_CALL_CALLED_NUMBER = '88901';
+const service_app_client_id = 'RzU4MTA1ODgtNDNkNy00MjkzLTg1ODktYTllNzY5YzIxY2YyYmI3ZTcwYTAtMGQ5_PF84_8c3c67f5-9dba-4be8-b058-76d7afe45521';
+const service_app_client_secret ='OGRhYjJhMGQtM2Q1OC00MTY5LWIzMTctZjJhNWU5NjRiMDkwNWJmMGRhMTctM2Ux_PF84_8c3c67f5-9dba-4be8-b058-76d7afe45521';
+let service_app_token ='C4ee6a4567567575c77686e27299fd6ed54e98615f039cb629c45a69c7e6a4286';
+let service_app_refresh_token = '7535a743a8778c8512cf7b551bc9a4d1ffbc9a3a8deb08213020f93050532683';
+const CLICK_TO_CALL_CALLED_NUMBER = '8951';
 const CLICK_TO_CALL_GUEST_NAME = 'Test app';
 const WEBEX_DISCOVERY_REGION = 'US-EAST';
 const WEBEX_DISCOVERY_COUNTRY = 'US';
